0ca82e82455ebf877cd66cd9a4ae7004426fd91a
Sowmya <158758138+Sowmyasitara@users.noreply.github.com>
2025-03-03 12:53:39 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "color-primary" : "#01012e",
+        "color-blue": "#243c5a",
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
